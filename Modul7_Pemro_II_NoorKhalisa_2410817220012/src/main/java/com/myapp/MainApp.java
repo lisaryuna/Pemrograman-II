aaf646da2e53a810cc288646fd385c8534e516ba
Lisa <fa037024@gmail.com>
2025-12-13 22:28:16 +0800

    modified againn

--- Modul7_Pemro_II_NoorKhalisa_2410817220012/src/main/java/com/myapp/MainApp.java
+++ Modul7_Pemro_II_NoorKhalisa_2410817220012/src/main/java/com/myapp/MainApp.java
@@ -12,7 +12,7 @@ public class MainApp extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("main-view.fxml"));
 
-        Scene scene = new Scene(fxmlLoader.load(), 650, 450);
+        Scene scene = new Scene(fxmlLoader.load(), 700, 450);
 
         stage.setTitle("Subscription Tracker");
         stage.setScene(scene);
